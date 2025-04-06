6cc6b5da0a416326d9325b77dc03ddd80ed02f8c
ShiQingQAQ <20108872@mail.wit.ie>
2025-04-06 01:27:25 +0100

    Completed Fan out implementation - added emailing logic

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -27,6 +27,11 @@ export class EDAAppStack extends cdk.Stack {
       const imageProcessQueue = new sqs.Queue(this, "img-created-queue", {
         receiveMessageWaitTime: cdk.Duration.seconds(10),
       });
+
+      const mailerQ = new sqs.Queue(this, "mailer-queue", {
+        receiveMessageWaitTime: cdk.Duration.seconds(10),
+      });  
+
       const newImageTopic = new sns.Topic(this, "NewImageTopic", {
         displayName: "New Image topic",
       }); 
@@ -45,6 +50,14 @@ export class EDAAppStack extends cdk.Stack {
     }
   );
 
+  const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
+    runtime: lambda.Runtime.NODEJS_16_X,
+    memorySize: 1024,
+    timeout: cdk.Duration.seconds(3),
+    entry: `${__dirname}/../lambdas/mailer.ts`,
+  });
+
+
   // S3 --> SQS
   imagesBucket.addEventNotification(
     s3.EventType.OBJECT_CREATED,
@@ -55,6 +68,11 @@ export class EDAAppStack extends cdk.Stack {
     new subs.SqsSubscription(imageProcessQueue)
   );
 
+  newImageTopic.addSubscription(
+    new subs.SqsSubscription(mailerQ)
+  );
+
+
 
  // SQS --> Lambda
   const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
@@ -62,11 +80,33 @@ export class EDAAppStack extends cdk.Stack {
     maxBatchingWindow: cdk.Duration.seconds(5),
   });
 
+  const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+    batchSize: 5,
+    maxBatchingWindow: cdk.Duration.seconds(5),
+  }); 
+
+
   processImageFn.addEventSource(newImageEventSource);
+
+  mailerFn.addEventSource(newImageMailEventSource);
+
 
   // Permissions
 
   imagesBucket.grantRead(processImageFn);
+
+  mailerFn.addToRolePolicy(
+    new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "ses:SendEmail",
+        "ses:SendRawEmail",
+        "ses:SendTemplatedEmail",
+      ],
+      resources: ["*"],
+    })
+  );
+
 
   // Output
   
